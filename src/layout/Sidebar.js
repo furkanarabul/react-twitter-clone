40cfccfe39430e37415c22a6b2dd1793c6470731
Furkan <arabulfurkan@gmail.com>
2021-05-12 01:24:08 +0200

    icons setup

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {HomeIcon} from "../icons/Icon"
+import {ExploreIcon} from "../icons/Icon"
+import {NotificationIcon} from "../icons/Icon"
+import {MessagesIcon} from "../icons/Icon"
+import {BookmarksIcon} from "../icons/Icon"
+import {ListsIcon} from "../icons/Icon"
+import {ProfileIcon} from "../icons/Icon"
+import {MoreIcon} from "../icons/Icon"
+
 
 const Sidebar = () => {
     return (
         <div className="w-72 bg-blue-200">
             <h1>Sidebar</h1>
+            <HomeIcon/>
+            <ExploreIcon/>
+            <NotificationIcon/>
+            <MessagesIcon/>
+            <BookmarksIcon/>
+            <ListsIcon/>
+            <ProfileIcon/>
+            <MoreIcon/>
         </div>
     )
 }
