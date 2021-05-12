43e115b5c20ae6d9821bf4123a31611e9961f4aa
Furkan <arabulfurkan@gmail.com>
2021-05-12 22:23:28 +0200

    refactor icons

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import {HomeIcon} from "../icons/Icon"
-import {ExploreIcon} from "../icons/Icon"
-import {NotificationIcon} from "../icons/Icon"
-import {MessagesIcon} from "../icons/Icon"
-import {BookmarksIcon} from "../icons/Icon"
-import {ListsIcon} from "../icons/Icon"
-import {ProfileIcon} from "../icons/Icon"
-import {MoreIcon} from "../icons/Icon"
-import {ImageIcon} from "../icons/Icon"
-import {PopularIcon} from "../icons/Icon"
-import {GifIcon} from "../icons/Icon"
-import {PollIcon} from "../icons/Icon"
-import {EmojiIcon} from "../icons/Icon"
-import {PlanIcon} from "../icons/Icon"
-import {SettingIcon} from "../icons/Icon"
-import {ReplyIcon} from "../icons/Icon"
-import {RetweetIcon} from "../icons/Icon"
-import {LikeIcon} from "../icons/Icon"
-import {ShareIcon} from "../icons/Icon"
+import  {
+HomeIcon,
+ExploreIcon,
+NotificationIcon,
+MessagesIcon,
+BookmarksIcon,
+ListsIcon,
+ProfileIcon,
+MoreIcon,
+ImageIcon,
+PopularIcon,
+GifIcon,
+PollIcon,
+EmojiIcon,
+PlanIcon,
+SettingIcon,
+ReplyIcon,
+RetweetIcon,
+LikeIcon,
+ShareIcon} from "../icons/Icon"
 
 
 
